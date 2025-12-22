f42e9313c95413ece45f57799d157a6bdec07863
Suraj Pal <surajpal7442@gmail.com>
2025-12-22 10:04:44 +0530

    first commit

---
+++ backend/src/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        console.log("MongoDB connected successfully");
+    }).catch((err)=> {
+        console.error("MongoDB connection error:", err.message);
+    });
+
+    module.exports = mongoose;
