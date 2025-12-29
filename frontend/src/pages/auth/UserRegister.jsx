e41d1c63c9f1aa15311bf8eca8e5bddc1c86aff2
Suraj Pal <surajpal7442@gmail.com>
2025-12-29 10:09:39 +0530

    Add authentication and registration for users and food partners - Created CreateFood and Home components for navigation. - Added CORS support in backend for cross-origin requests. - Updated styles for authentication forms and shared components.

--- frontend/src/pages/auth/UserRegister.jsx
+++ frontend/src/pages/auth/UserRegister.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/auth-shared.css';
+import axios from'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 const UserRegister = () => {
-    const handleSubmit = (e) => {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-}
+
+        const firstName = e.target.firstName.value;
+        const lastName = e.target.lastName.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const response = await axios.post("http://localhost:3000/api/auth/user/register",{
+            fullName: firstName + " " + lastName,
+            email,
+            password
+        }, {
+            withCredentials: true
+        })
+
+        console.log(response.data);
+
+        navigate("/"); // Redirect to home after successful registration
+};
 
     return (
         <div className="auth-page-wrapper">
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,8 @@ import ChooseRegister from '../pages/auth/ChooseRegister';
 import UserLogin from '../pages/auth/UserLogin';
 import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister';
 import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin';
+import Home from '../pages/general/Home';
+import CreateFood from '../pages/food-partner/CreateFood';
 
 const AppRoutes = () => {
     return (
@@ -15,6 +17,8 @@ const AppRoutes = () => {
                 <Route path="/user/login" element={<UserLogin />} />
                 <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
                 <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/create-food" element={<CreateFood />} />
             </Routes>
         </Router>
     )
